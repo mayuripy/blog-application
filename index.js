128b0f50f63234841eab226e75af1b9400eccdce
mayuripy <115979827+mayuripy@users.noreply.github.com>
2023-10-01 15:45:50 +0530

    ...

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 const express = require("express")
 
-const { } =require("express-graphql")
+const { graphqlHTTP } =require("express-graphql");
+const { GraphQLObjectType, GraphQLID, GraphQLString } = require("graphql");
 
 const app = express();
+
+const UserList = [
+
+]
+
+const UserType = new GraphQLObjectType({
+     name:"userType",
+     fields: () => ({
+        id: {type: GraphQLID},
+        name: {type:GraphQLString },
+        email: {type: GraphQLString},
+     }),
+
+})
+
+const RoorQuery = new GraphQLObjectType({
+    name:"RootQuery",
+    fields:{
+       users: {
+        type: GraphaQLList(UserType),
+        resolve(parent,args) {
+            return[],
+        },
+
+       },
+    },
+})
+
+app.use("/graphql",graphqlHTTP({schema:{},graphiql: true}));
 
 app.listen(5000, () => console.log("server Running"));
