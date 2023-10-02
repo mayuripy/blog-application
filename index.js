86fa43a0c039b38b72caa0e611fb69e86fcf8308
mayuripy <115979827+mayuripy@users.noreply.github.com>
2023-10-02 20:46:22 +0530

    ...

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const { GraphQLObjectType, GraphQLID, GraphQLString } = require("graphql");
 
 const app = express();
 
-const UserList = [
-
+const UsersList = [
+   {id: "1",name: "mayuri",email:"mayuri@test.com"},
+   {id: "1",name: "minal",email:"minal@test.com"},
+   {id: "1",name: "adhi",email:"adhi@test.com"},
 ]
 
 const UserType = new GraphQLObjectType({
@@ -19,13 +21,13 @@ const UserType = new GraphQLObjectType({
 
 })
 
-const RoorQuery = new GraphQLObjectType({
+const RootQuery = new GraphQLObjectType({
     name:"RootQuery",
     fields:{
        users: {
         type: GraphaQLList(UserType),
-        resolve(parent,args) {
-            return[],
+        resolve() {
+            return UsersList;
         },
 
        },
